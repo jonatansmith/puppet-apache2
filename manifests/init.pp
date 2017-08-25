ee5a5d409ad4e015fd48e92a90bd8cbf23119bbc
jonatansmith <jonatansmith@gmail.com>
2017-08-25 10:59:58 -0300

    Versão do Vini

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,9 @@
-#Class: Apache2
-class apache2 {
-
-  package { 'apache2':
-    ensure => 'present',
-  }
-
-  service { 'apache2':
-    ensure => 'running',
-  }
-
+package { 'apache2':
+  ensure => 'present',
 }
+
+service { 'apache2':
+  ensure => 'running',
+}
+
+Package['apache2'] -> Service['apache2']
